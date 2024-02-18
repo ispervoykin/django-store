388a8f198b712d20486848294aff8bd34548dce0
ispervoykin <ilya.perv2@gmail.com>
2024-02-19 00:39:52 +0300

    Add fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request: HttpRequest):
+    categories = Categories.objects.all()
+
     context = {
         "title": "Home - Главная",
         "content": "Магазин мебели HOME",
+        "categories": categories,
     }
 
     return render(request, "main/index.html", context)
