a1ddf46ad8682ca3fb68f37d82573b6fc74a596a
ispervoykin <ilya.perv2@gmail.com>
2024-02-18 21:11:24 +0300

    Create the main app, add index and about pages

--- main/views.py
+++ main/views.py
@@ -4,15 +4,18 @@ from django.shortcuts import render
 
 def index(request: HttpRequest):
     context = {
-        "title": "Home",
-        "content": "Главная страница магазина",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": True,
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
     }
 
     return render(request, "main/index.html", context)
 
 
 def about(request: HttpRequest):
-    return HttpResponse("Home Page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Описание магазина",
+    }
+
+    return render(request, "main/about.html", context)
