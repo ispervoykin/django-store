7b1e97ed5bab4081c368a88791bd62f5304d4efd
ispervoykin <ilya.perv2@gmail.com>
2024-02-19 19:52:17 +0300

    Add the second pagination with GET params

--- goods/views.py
+++ goods/views.py
@@ -4,7 +4,9 @@ from django.core.paginator import Paginator
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = int(request.GET.get("page", 1))
+
     if category_slug == "all":
         goods = get_list_or_404(Products.objects.all())
     else:
