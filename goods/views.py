5a92bd371c7c776abe638969d2c30ead1662600a
ispervoykin <ilya.perv2@gmail.com>
2024-02-20 22:29:15 +0300

    Add login form

--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_list_or_404, redirect, render
 from django.core.paginator import Paginator
+from django.urls import reverse
 
 from goods.models import Products
 from goods.utils import q_search
@@ -12,7 +14,7 @@ def catalog(request, category_slug=None):
     query = request.GET.get("q", None)
 
     if not query and not category_slug:
-        return redirect("main:index")
+        return HttpResponseRedirect(reverse("main:index"))
 
     if category_slug == "all":
         goods = Products.objects.all()
--- users/views.py
+++ users/views.py
@@ -1,8 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {"title": "Home - Авторизация"}
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
+    context = {"title": "Home - Авторизация", "form": form}
     return render(request, "users/login.html", context)
 
 
@@ -16,4 +33,5 @@ def profile(request):
     return render(request, "users/profile.html", context)
 
 
-def logout(request): ...
+def logout(request):
+    pass
