bf7e45f402b2944c2700fc8332fbbb8d6d9d1f0b
ispervoykin <ilya.perv2@gmail.com>
2024-02-21 12:33:56 +0300

    Include cart templates

---
+++ carts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def cart_add(request, product_id):
+    pass
+
+
+def cart_change(request, product_id):
+    pass
+
+
+def cart_delete(request, product_id):
+    pass
