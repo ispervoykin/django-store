00a4de1b97db958d6c2f83512fb43389c4f8aa70
ispervoykin <ilya.perv2@gmail.com>
2024-02-24 20:31:14 +0300

    Add remove from cart

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ app_name = "carts"
 urlpatterns = [
     path("cart_add/", views.cart_add, name="cart_add"),
     path("cart_change/", views.cart_change, name="cart_change"),
-    path("cart_delete/", views.cart_delete, name="cart_delete"),
+    path("cart_remove/", views.cart_remove, name="cart_remove"),
 ]
