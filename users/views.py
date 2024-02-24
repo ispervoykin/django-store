ee566ce554a0ea8c0ce1357e6a0ffc7811f4e6e1
ispervoykin <ilya.perv2@gmail.com>
2024-02-24 21:31:02 +0300

    Add sessions for anonymous users

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib import auth, messages
 from django.urls import reverse
+from carts.models import Cart
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
@@ -14,9 +15,14 @@ def login(request):
             username = request.POST["username"]
             password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get("next", None)
                 if redirect_page and redirect_page != reverse("user:logout"):
@@ -35,7 +41,11 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             user = form.save()
+            session_key = request.session.session_key
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
             messages.success(
                 request,
                 f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт",
